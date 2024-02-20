8573dd548adc37c8ed62ae386d8bb78e3df92466
Kloosy <antonefrimov06@gmail.com>
2024-02-20 14:54:07 +0300

    Добавьте файлы проекта.

---
+++ DZ2002/DZ2002.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class SqVector
+{
+private:
+    vector<int> data;
+
+public:
+
+    void addSq() 
+    {
+        for (int i = 0; i < data.size(); ++i) 
+        {
+            data[i] = (i + 1) * (i + 1);
+        }
+    }
+
+    void displayVector(ostream& os) const 
+    {
+        for (int value : data) 
+        {
+            os << value << " ";
+        }
+        os << endl;
+    }
+
+    SqVector()
+    {
+        data.resize(10);
+        addSq();
+    }
+
+    ~SqVector()
+    {
+
+    }
+};
+
+int main()
+{
+    SqVector squareVector;
+    squareVector.displayVector(cout);
+}
